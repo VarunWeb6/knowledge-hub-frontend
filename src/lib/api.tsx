9583d399e63baef42c3818b04eace763adf2af99
Varunweb6 <varunweb6@gmail.com>
2025-08-19 21:44:44 +0530

    feat : added to prod

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set your backend URL
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://backend-knowledge-hub.onrender.com/api/';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
